7e896ba6a17219e27a4832da72e86df264d39cf1
Yann VERY <yann.very@gmail.com>
2014-01-30 16:38:35 +0100

    Add UsdObject as return of all web methods

--- App_Code/Jump2UsdL1.cs
+++ App_Code/Jump2UsdL1.cs
@@ -20,22 +20,19 @@ public class Jump2UsdL1 : System.Web.Services.WebService
     private string myHandle = null;
     private string methodName = null;
     private string[] myHandleAttr = { "persistent_id" };
-    private XmlDocument docObject;
-    private XmlNode XmlReturn;
+    UsdObject usdObject;
     private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
 
     public Jump2UsdL1()
     {
 
-        this.docObject = new XmlDocument();     
+        this.usdObject = new UsdObject();    
     }
 
     public void initializeResource(string methodName)
     {
-        this.docObject.LoadXml("<" + methodName + "Response><" + methodName + "Result></" + methodName + "Result></" + methodName + "Response>");
-        this.XmlReturn = this.docObject.SelectNodes("/" + methodName + "Response/" + methodName + "Result")[0];
-
+        this.methodName = methodName;
         try
         {
             this.myUsdService = new USD_WebService();
@@ -43,8 +40,10 @@ public class Jump2UsdL1 : System.Web.Services.WebService
         }
         catch (Exception e)
         {
-            this.myWsTools.CreateXmlNode("ReturnCode", "ERR", this.docObject, this.XmlReturn);
-            this.myWsTools.CreateXmlNode("ReturnMessage", e.Message, this.docObject, this.XmlReturn);
+            this.usdObject.ReturnCode = "ERR";
+            this.usdObject.ReturnMessage = e.Message;
+            this.usdObject.Description = "Can't initialize resource";
+            this.usdObject.Success = false;
         }
     }
 
@@ -57,12 +56,12 @@ public class Jump2UsdL1 : System.Web.Services.WebService
         }
         catch (Exception e)
         {
-            setXmlReturn("Log in failed", "-1", e.Message, "false");
+            setReturn("Log in failed", "-1", e.Message, false);
         }
         return sid;
     }
 
-    public void setXmlReturn(string description, string code, string message, string success)
+    public void setReturn(string description, string code, string message, bool success)
     {
         string logMessage = "";
         string logDescription = "";
@@ -80,10 +79,10 @@ public class Jump2UsdL1 : System.Web.Services.WebService
             logDescription = " - Description : " + description;
         }
         this.myWsTools.log(level, this.methodName + " - " + this.mySID + logDescription + logMessage);
-        this.myWsTools.CreateXmlNode("Description", description, this.docObject, this.XmlReturn);
-        this.myWsTools.CreateXmlNode("ReturnCode", code, this.docObject, this.XmlReturn);
-        this.myWsTools.CreateXmlNode("ReturnMessage", message, this.docObject, this.XmlReturn);
-        this.myWsTools.CreateXmlNode("success", success, this.docObject, this.XmlReturn);
+        this.usdObject.ReturnCode = code;
+        this.usdObject.ReturnMessage = message;
+        this.usdObject.Description = description;
+        this.usdObject.Success = success;
     }
 
     public Boolean validatePresence(string param_name, string param_value)
@@ -91,7 +90,7 @@ public class Jump2UsdL1 : System.Web.Services.WebService
         Boolean myReturn = true;
         if (param_value == null)
         {
-            setXmlReturn("Invalid parameter", "1", "parameter " + param_name + " is null", "false");
+            setReturn("Invalid parameter", "1", "parameter " + param_name + " is null", false);
             myReturn = false;
         }
         return myReturn;
@@ -102,14 +101,79 @@ public class Jump2UsdL1 : System.Web.Services.WebService
         Boolean myReturn = true;
         if (String.IsNullOrEmpty(param_value))
         {
-            setXmlReturn("Invalid parameter", "2", "parameter " + param_name + " is null or empty", "false");
+            setReturn("Invalid parameter", "2", "parameter " + param_name + " is null or empty", false);
             myReturn = false;
         }
         return myReturn;
     }
 
     [WebMethod]
-    public XmlNode jumpAcknowledge(string username, string password, string ref_num, string jump_num)
+    public UsdObject attachEvent(string username, string password, string name, string crpersid)
+    {
+        initializeResource("attachEvent");
+        string myResult;
+        this.myWsTools.log("INFO", this.methodName + " - " + this.mySID + " - username : " + username + " - password : " + password + " - name : " + name);
+
+        // Validate parameters
+        if (!(validatePresenceNotEmpty("username", username))) return this.usdObject;
+        if (!(validatePresence("password", password))) return this.usdObject;
+        if (!(validatePresenceNotEmpty("name", name))) return this.usdObject;
+        if (!(validatePresenceNotEmpty("crpersid", crpersid))) return this.usdObject;
+
+        this.mySID = login(username, password);
+        if (this.mySID == 0)
+        {
+            return this.usdObject;
+        }
+        this.myWsTools.log("INFO", this.methodName + " - " + this.mySID + " - SID : " + this.mySID);
+
+        //Get Handle
+        try
+        {
+            myResult = this.myUsdService.doSelect(this.mySID, "evt", "sym = '" + name + "'", -1, this.myHandleAttr);
+            XmlDocument myValue = new XmlDocument();
+            myValue.LoadXml(myResult);
+            myHandle = myValue.SelectSingleNode("//UDSObject/Handle/text()").Value;
+        }
+        catch (Exception e)
+        {
+            setReturn("Can't find event", "-1", e.Message, false);
+            return this.usdObject;
+        }
+        this.myWsTools.log("INFO", this.methodName + " - " + this.mySID + " - Handle : " + myHandle);
+        
+        // create object
+         string[] myAttrVals = {
+                "obj_id",
+                crpersid,
+                "event_tmpl",
+                myHandle,
+            };
+        string[] myAttributes = {
+                "persistent_id"
+            };
+        string createObjectReturn = "";
+        string newHandle= "";
+        try
+        {
+            this.myUsdService.createObject(this.mySID, "atev", myAttrVals, myAttributes, ref createObjectReturn, ref newHandle);
+            setReturn("Attach event done", "0", newHandle, true);
+        }
+        catch (Exception e)
+        {
+            setReturn("Can't attach event", "-1", e.Message, false);
+            return this.usdObject;
+        }
+        // logout
+        if (this.mySID != 0)
+        {
+            this.myUsdService.logout(this.mySID);
+        }
+        return this.usdObject;
+    }
+
+    [WebMethod]
+    public UsdObject jumpAcknowledge(string username, string password, string ref_num, string jump_num)
     {
         this.methodName = "jumpAcknowledge";
         initializeResource(methodName);
@@ -117,10 +181,10 @@ public class Jump2UsdL1 : System.Web.Services.WebService
         this.myWsTools.log("INFO", this.methodName + " - " + this.mySID + " - username : " + username + " - password : " + password + " - ref_num : " + ref_num + " - jump_num : " + jump_num);
 
         // Validate parameters
-        if (!(validatePresenceNotEmpty("username", username))) return this.docObject;
-        if (!(validatePresence("password", password))) return this.docObject;
-        if (!(validatePresenceNotEmpty("ref_num", ref_num))) return this.docObject;
-        if (!(validatePresenceNotEmpty("jump_num", jump_num))) return this.docObject;
+        if (!(validatePresenceNotEmpty("username", username))) return this.usdObject;
+        if (!(validatePresence("password", password))) return this.usdObject;
+        if (!(validatePresenceNotEmpty("ref_num", ref_num))) return this.usdObject;
+        if (!(validatePresenceNotEmpty("jump_num", jump_num))) return this.usdObject;
 
         DateTime saveNow = DateTime.Now;
         TimeSpan elapsedTime = saveNow - Epoch;
@@ -128,7 +192,7 @@ public class Jump2UsdL1 : System.Web.Services.WebService
         this.mySID = login(username, password);
         if (this.mySID == 0)
         {
-            return this.docObject;
+            return this.usdObject;
         }
         this.myWsTools.log("INFO", this.methodName + " - " + this.mySID + " - SID : " + this.mySID);
         
@@ -142,30 +206,34 @@ public class Jump2UsdL1 : System.Web.Services.WebService
         }
         catch (Exception e)
         {
-            setXmlReturn("Can't find incident", "3", e.Message, "false");
-            return this.docObject;
+            setReturn("Can't find incident", "3", e.Message, false);
+            return this.usdObject;
         }
         this.myWsTools.log("INFO", this.methodName + " - " + this.mySID + " - Handle : " + myHandle);
         
         //Update CR
         try
         {
-            myResult = this.myUsdService.updateObject(this.mySID, myHandle, new string[] { "zjump", jump_num, "zjump_majdt", elapsedTime.TotalSeconds.ToString() }, new string[0]);
-            setXmlReturn("Acknowledge creation of request", "0", "", "true");
+            int totalsec = (int)elapsedTime.TotalSeconds;
+            //myResult = this.myUsdService.updateObject(this.mySID, myHandle, new string[] { "zjump", jump_num, "zjump_majdt", totalsec.ToString() }, new string[0]);
+            myResult = this.myUsdService.updateObject(this.mySID, myHandle, new string[] { "zjump", jump_num, }, new string[0]);
+            setReturn("Acknowledge creation of request", "0", "", true);
         }
         catch (Exception e)
         {
-            setXmlReturn("Incident can't be updated", "4", e.Message, "false");
-            return this.docObject;
+            setReturn("Incident can't be updated", "4", e.Message, false);
+            return this.usdObject;
         }
 
         this.myWsTools.log("INFO", this.methodName + " - " + this.mySID + " - Incident updated");
+        
         // logout
         if (this.mySID != 0)
         {
             this.myUsdService.logout(this.mySID);
         }
 
-        return this.docObject;
+        return this.usdObject;
     }
+
 }
